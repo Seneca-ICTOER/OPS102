f2aeaf2f9208e32c5c481f7b923e72dd375c6e5a
Chris Johnson <chris.johnson@senecacollege.ca>
2023-09-02 17:47:22 -0400

    Adds course code and title changes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'OPS102 - Operating Systems for Programmers',
+  tagline: 'OPS102',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/OPS102/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'OPS102',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'OPS102 - Operating Systems for Programmers',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'OPS102 - Operating Systems for Programmers',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/OPS102/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
